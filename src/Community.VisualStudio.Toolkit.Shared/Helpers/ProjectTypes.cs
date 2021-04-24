28dac01a7b01cad9ec43816fda312c56c9654af7
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-04-24 07:56:11 -0600

    Added additional project types.

--- src/Community.VisualStudio.Toolkit.Shared/Helpers/ProjectTypes.cs
+++ src/Community.VisualStudio.Toolkit.Shared/Helpers/ProjectTypes.cs
@@ -5,31 +5,208 @@
     /// </summary>
     public static class ProjectTypes
     {
-        /// <summary>The project type of ASP.NET Core projects.</summary>
-        public const string ASPNET_Core = "{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}";
+        /// <summary>The project type of Asp.net Core</summary>
+        public const string ASPNET_CORE = "{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}";
+
+        /// <summary>The project type of Asp.net MVC v1</summary>
+        public const string ASPNET_MVC_1 = "{603C0E0B-DB56-11DC-BE95-000D561079B0}";
+
+        /// <summary>The project type of Asp.net MVC v2</summary>
+        public const string ASPNET_MVC_2 = "{F85E285D-A4E0-4152-9332-AB1D724D3325}";
+
+        /// <summary>The project type of Asp.net MVC v3</summary>
+        public const string ASPNET_MVC_3 = "{E53F8FEA-EAE0-44A6-8774-FFD645390401}";
+
+        /// <summary>The project type of Asp.net MVC v4</summary>
+        public const string ASPNET_MVC_4 = "{E3E379DF-F4C6-4180-9B81-6769533ABE47}";
+
+        /// <summary>The project type of Asp.net MVC v5</summary>
+        public const string ASPNET_MVC_5 = "{349C5851-65DF-11DA-9384-00065B846F21}";
+
+        /// <summary>The project type of C#</summary>
+        public const string CSHARP = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+
+        /// <summary>The project type of C++</summary>
+        public const string C_PLUS_PLUS = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
+
+        /// <summary>The project type of Database</summary>
+        public const string DATABASE = "{A9ACE9BB-CECE-4E62-9AA4-C7E7C5BD2124}";
+
+        /// <summary>The project type of Database (Other)</summary>
+        public const string DATABASE_OTHER = "{4F174C21-8C12-11D0-8340-0000F80270F8}";
+
+        /// <summary>The project type of Deployment CAB</summary>
+        public const string DEPLOYMENT_CAB = "{3EA9E505-35AC-4774-B492-AD1749C4943A}";
+
+        /// <summary>The project type of Deployment Merge Module</summary>
+        public const string DEPLOYMENT_MERGE_MODULE = "{06A35CCD-C46D-44D5-987B-CF40FF872267}";
+
+        /// <summary>The project type of Deployment Setup</summary>
+        public const string DEPLOYMENT_SETUP = "{978C614F-708E-4E1A-B201-565925725DBA}";
+
+        /// <summary>The project type of Deployment Smart Device CAB</summary>
+        public const string DEPLOYMENT_SMART_DEVICE_CAB = "{AB322303-2255-48EF-A496-5904EB18DA55}";
+
+        /// <summary>The project type of Distributed System</summary>
+        public const string DISTRIBUTED_SYSTEM = "{F135691A-BF7E-435D-8960-F99683D2D49C}";
 
         /// <summary>The project type of .NET Core projects.</summary>
         public const string DOTNET_Core = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
 
+        /// <summary>The project type of Dynamics 2012 AX</summary>
+        public const string DYNAMICS_2012_AX = "{BF6F8E12-879D-49E7-ADF0-5503146B24B8}";
+
+        /// <summary>The project type of F#</summary>
+        public const string F_SHARP = "{F2A71F9B-5D33-465A-A702-920D77279786}";
+
+        /// <summary>The project type of J#</summary>
+        public const string J_SHARP = "{E6FDF86B-F3D1-11D4-8576-0002A516ECE8}";
+
+        /// <summary>The project type of Legacy Smart Device C#</summary>
+        public const string LEGACY_SMART_DEVICE_CSHARP = "{20D4826A-C6FA-45DB-90F4-C717570B9F32}";
+
+        /// <summary>The project type of Legacy Smart Device VB.net</summary>
+        public const string LEGACY_SMART_DEVICE_VB = "{CB4CE8C6-1BDB-4DC7-A4D3-65A1999772F8}";
+
+        /// <summary>The project type of Micro Framework</summary>
+        public const string MICRO_FRAMEWORK = "{b69e3092-b931-443c-abe7-7e7b65f2a37f}";
+
         /// <summary>The project type of Misc projects.</summary>
         public const string MISC = "{66A2671D-8FB5-11D2-AA7E-00C04F688DDE}";
+
+        /// <summary>The project type of Model-View-Controller (MVC) v2</summary>
+        public const string MVC_2 = "{F85E285D-A4E0-4152-9332-AB1D724D3325}";
+
+        /// <summary>The project type of Model-View-Controller (MVC) v3</summary>
+        public const string MVC_3 = "{E53F8FEA-EAE0-44A6-8774-FFD645390401}";
+
+        /// <summary>The project type of Model-View-Controller (MVC) v4</summary>
+        public const string MVC_4 = "{E3E379DF-F4C6-4180-9B81-6769533ABE47}";
+
+        /// <summary>The project type of Model-View-Controller (MVC) v5</summary>
+        public const string MVC_5 = "{349C5851-65DF-11DA-9384-00065B846F21}";
+
+        /// <summary>The project type of Mono Android</summary>
+        public const string MONO_ANDROID = "{EFBA0AD7-5A72-4C68-AF49-83D382785DCF}";
+
+        /// <summary>The project type of Mono Touch</summary>
+        public const string MONO_TOUCH = "{6BC8ED88-2882-458C-8E55-DFD12B67127B}";
+
+        /// <summary>The project type of Mono Touch Binding</summary>
+        public const string MONOTOUCH_BINDING = "{F5B4F3BC-B597-4E2B-B552-EF5D8A32436F}";
 
         /// <summary>The project type of Node.js projects.</summary>
         public const string NODE_JS = "{9092AA53-FB77-4645-B42D-1CCCA6BD08BD}";
 
-        /// <summary>The project type of a solution folder.</summary>
+        /// <summary>The project type of Protable Class Library</summary>
+        public const string PCL = "{786C830F-07A1-408B-BD7F-6EE04809D6DB}";
+
+        /// <summary>The project type of Solution Folder</summary>
         public const string SOLUTION_FOLDER = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}";
+
+        /// <summary>The project type of Sharepoint C#</summary>
+        public const string SHAREPOINT_CSHARP = "{593B0543-81F6-4436-BA1E-4747859CAAE2}";
+
+        /// <summary>The project type of Sharepoint VB.net</summary>
+        public const string SHAREPOINT_VB = "{EC05E597-79D4-47f3-ADA0-324C4F7C7484}";
+
+        /// <summary>The project type of Sharepoint Workflow</summary>
+        public const string SHAREPOINT_WORKFLOW = "{F8810EC1-6754-47FC-A15F-DFABD2E3FA90}";
+
+        /// <summary>The project type of Silverlight</summary>
+        public const string SILVERLIGHT = "{A1591282-1198-4647-A2B1-27E5FF5F6F3B}";
+
+        /// <summary>The project type of Smart Device C#</summary>
+        public const string SMART_DEVICE_CSHARP = "{4D628B5B-2FBC-4AA6-8C16-197242AEB884}";
+
+        /// <summary>The project type of Smart Device VB.net</summary>
+        public const string SMART_DEVICE_VB = "{68B1623D-7FB9-47D8-8664-7ECEA3297D4F}";
+
+        /// <summary>The project type of Solution Folder (Other)</summary>
+        public const string SOLUTION_FOLDER_OTHER = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
 
         /// <summary>The project type of SQL Server Data projects.</summary>
         public const string SSDT = "{00d1a9c2-b5f0-4af3-8072-f6c62b433612}";
 
+        /// <summary>The project type of Test</summary>
+        public const string TEST = "{3AC096D0-A1C2-E12C-1390-A8335801FDAB}";
+
+        /// <summary>The project type of Universal Windows Class Library</summary>
+        public const string UNIVERSAL_WINDOWS_CLASS_LIBRARY = "{A5A43C5B-DE2A-4C0C-9213-0A381AF9435A}";
+
         /// <summary>The project type of UWP app projects.</summary>
         public const string UNIVERSAL_APP = "{262852C6-CD72-467D-83FE-5EEB1973A190}";
 
-        /// <summary>The project type of Web Application projects.</summary>
-        public const string WAP = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+        /// <summary>The project type of VB.net</summary>
+        public const string VB = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
 
-        /// <summary>The project type of the legacy website projects.</summary>
-        public const string WEBSITE_PROJECT = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+        /// <summary>The project type of Visual Database Tools</summary>
+        public const string VISUAL_DATABASE_TOOLS = "{C252FEB5-A946-4202-B1D4-9916A0590387}";
+
+        /// <summary>The project type of Visual Studio 2015 Installer Project</summary>
+        public const string VISUAL_STUDIO_2015_INSTALLER_PROJECT = "{54435603-DBB4-11D2-8724-00A0C9A8B90C}";
+
+        /// <summary>The project type of Visual Studio Tools for Applications</summary>
+        public const string VSTA = "{A860303F-1F3F-4691-B57E-529FC101A107}";
+
+        /// <summary>The project type of Visual Studio Tools for Office</summary>
+        public const string VSTO = "{BAA0C2D2-18E2-41B9-852F-F413020CAA33}";
+
+        /// <summary>The project type of Web Application</summary>
+        public const string WEB_APPLICATION = "{349C5851-65DF-11DA-9384-00065B846F21}";
+
+        /// <summary>The project type of Website</summary>
+        public const string WEBSITE = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+
+        /// <summary>The project type of Windows C#</summary>
+        public const string WINDOWS_CSHARP = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+
+        /// <summary>The project type of Windows VB.net</summary>
+        public const string WINDOWS_VB = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
+
+        /// <summary>The project type of Windows C++</summary>
+        public const string WINDOWS_C_PLUS_PLUS = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
+
+        /// <summary>The project type of </summary>
+        public const string WCF = "{3D9AD99F-2412-4246-B90B-4EAA41C64699}";
+
+        /// <summary>The project type of Windows Phone 8</summary>
+        public const string WINDOWS_PHONE_8_BLANK = "{76F1466A-8B6D-4E39-A767-685A06062A39}";
+
+        /// <summary>The project type of Windows Phone 8 C#</summary>
+        public const string WINDOWS_PHONE_8_CSHARP = "{C089C8C0-30E0-4E22-80C0-CE093F111A43}";
+
+        /// <summary>The project type of Windows Phone 8 VB.net</summary>
+        public const string WINDOWS_PHONE_8_VB = "{DB03555F-0C8B-43BE-9FF9-57896B3C5E56}";
+
+        /// <summary>The project type of WCF</summary>
+        public const string WPF = "{60DC8134-EBA5-43B8-BCC9-BB4BC16C2548}";
+
+        /// <summary>The project type of Windows Store Metro</summary>
+        public const string WINDOWS_STORE_METRO = "{BC8A1FFA-BEE3-4634-8014-F334798102B3}";
+
+        /// <summary>The project type of Workflow C#</summary>
+        public const string WORKFLOW_CSHARP = "{14822709-B5A1-4724-98CA-57A101D1B079}";
+
+        /// <summary>The project type of Workflow VB.net</summary>
+        public const string WORKFLOW_VB = "{D59BE175-2ED0-4C54-BE3D-CDAA9F3214C8}";
+
+        /// <summary>The project type of Workflow Foundation</summary>
+        public const string WORKFLOW_FOUNDATION = "{32F31D43-81CC-4C15-9DE6-3FC5453562B6}";
+
+        /// <summary>The project type of Xamarin Android</summary>
+        public const string XAMARIN_ANDROID = "{EFBA0AD7-5A72-4C68-AF49-83D382785DCF}";
+
+        /// <summary>The project type of Xamaric IOS</summary>
+        public const string XAMARIN_IOS = "{6BC8ED88-2882-458C-8E55-DFD12B67127B}";
+
+        /// <summary>The project type of XNA Windows</summary>
+        public const string XNA_WINDOWS = "{6D335F3A-9D43-41b4-9D22-F6F17C4BE596}";
+
+        /// <summary>The project type of XNA Xbox</summary>
+        public const string XNA_XBOX = "{2DF5C3F4-5A5F-47a9-8E94-23B4456F55E2}";
+
+        /// <summary>The project type of XNA Zune</summary>
+        public const string XNA_ZUNE = "{D399B71A-8929-442a-A9AC-8BEC78BB2433}";
     }
 }
