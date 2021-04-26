084e75a1cad56c3feb99aadddcbd187c02f8281e
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-04-26 14:08:06 -0700

    Added CodeBase parameter /cc #25

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 // Without it, Visual Studio is unable to find the assembly and the extension will fail to load.
 using Microsoft.VisualStudio.Shell;
 
-[assembly: ProvideCodeBase(AssemblyName = "Community.VisualStudio.Toolkit")]
+[assembly: ProvideCodeBase(AssemblyName = "Community.VisualStudio.Toolkit",
+                           CodeBase = "%PackageFolder%\\Community.VisualStudio.Toolkit.dll")]
