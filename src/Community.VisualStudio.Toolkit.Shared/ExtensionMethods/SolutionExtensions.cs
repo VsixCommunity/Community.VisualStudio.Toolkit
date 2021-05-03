1bbd2828aa71c940b9dee5526cfe9639a71419b7
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-05-03 15:37:48 -0600

    Added extension methods for saving the solution.

--- src/Community.VisualStudio.Toolkit.Shared/ExtensionMethods/SolutionExtensions.cs
+++ src/Community.VisualStudio.Toolkit.Shared/ExtensionMethods/SolutionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
 
 namespace EnvDTE
 {
@@ -28,6 +30,39 @@ namespace EnvDTE
                 // Returns 'true' if the number of failed projects == 0
                 buildTaskCompletionSource.TrySetResult(dte.Solution.SolutionBuild.LastBuildInfo == 0);
             }
+        }
+
+        /// <summary>
+        /// Saves the solution
+        /// Although this method is asynchronous, the saving of the solution is still synchronous and is performed on the main UI thread.
+        /// </summary>
+        /// <param name="solution"></param>
+        /// <returns></returns>
+        public static async Task SaveAsync(this Solution solution)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (solution == null)
+                throw new ArgumentNullException(nameof(solution));
+
+            solution.SaveAs(solution.FileName);
+        }
+
+        /// <summary>
+        /// Saves the solution with the specified name
+        /// Although this method is asynchronous, the saving of the solution is still synchronous and is performed on the main UI thread.
+        /// </summary>
+        /// <param name="solution"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static async Task SaveAsAsync(this Solution solution, string fileName)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (solution == null)
+                throw new ArgumentNullException(nameof(solution));
+
+            solution.SaveAs(fileName);
         }
     }
 }
