93db480ab74b26e98a305139d64f52f56184d87b
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-05-14 16:02:32 -0700

    Added TryExecuteCommand extension method

---
+++ src/Community.VisualStudio.Toolkit.Shared/ExtensionMethods/DteExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell;
+
+namespace EnvDTE
+{
+    /// <summary>Extension methods for the DTE.</summary>
+    public static class DteExtensions
+    {
+        /// <summary>
+        /// Builds the solution asynchronously
+        /// </summary>
+        /// <returns>Returns 'true' if successful</returns>
+        public static bool TryExecuteCommand(this _DTE dte, string commandName, string commandArgs = "")
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                Command? cmd = dte.Commands.Item(commandName);
+
+                if (cmd != null && cmd.IsAvailable)
+                {
+                    dte.ExecuteCommand(commandName, commandArgs);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+            }
+
+            return false;
+        }
+    }
+}
