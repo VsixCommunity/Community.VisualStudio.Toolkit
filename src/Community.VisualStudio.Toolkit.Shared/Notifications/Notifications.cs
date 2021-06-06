bc72d078fbd586f3e782db2f1747865988bfb44d
Greg Ingram <greg.ingram@carrier.com>
2021-06-05 20:43:35 -0400

    Summary updates.

--- src/Community.VisualStudio.Toolkit.Shared/Notifications/Notifications.cs
+++ src/Community.VisualStudio.Toolkit.Shared/Notifications/Notifications.cs
@@ -21,15 +21,15 @@ namespace Community.VisualStudio.Toolkit
         public Task<IVsTaskStatusCenterService> GetTaskStatusCenterAsync() => VS.GetServiceAsync<SVsTaskStatusCenterService, IVsTaskStatusCenterService>();
 #endif
 
-        /// <summary>The Infobar is often referred to as the 'yellow' or 'gold' bar.</summary>
-        /// <returns>Cast return object to <see cref="IVsInfoBarUIFactory"/></returns>
+        /// <summary>The <see cref="InfoBar"/> is often referred to as the 'yellow' or 'gold' bar.</summary>
+        /// <returns>Cast return object to <see cref="IVsInfoBarUIFactory"/>.</returns>
         public Task<object> GetInfoBarUIFactoryAsync() => VS.GetServiceAsync<SVsInfoBarUIFactory, object>();
 
         /// <summary>Used for background tasks that needs to block the UI if they take longer than the specified seconds.</summary>
-        /// <returns>Cast return object to <see cref="IVsThreadedWaitDialogFactory"/></returns>
+        /// <returns>Cast return object to <see cref="IVsThreadedWaitDialogFactory"/>.</returns>
         public Task<object> GetThreadedWaitDialogAsync() => VS.GetServiceAsync<SVsThreadedWaitDialogFactory, object>();
 
-        /// <summary>Used to write log messaged to the ActivityLog.xml file.</summary>
+        /// <summary>Used to write log messages to the ActivityLog.xml file.</summary>
         public Task<IVsActivityLog> GetActivityLogAsync() => VS.GetServiceAsync<SVsActivityLog, IVsActivityLog>();
     }
 }
