1058e127c058194aa4f7f0c331a287d7d26c3a09
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-07-13 11:52:50 -0600

    Updated SToolkitServiceProvider to include a type constraint.

--- src/Community.VisualStudio.Toolkit.Shared/DependencyInjection/SToolkitServiceProvider.cs
+++ src/Community.VisualStudio.Toolkit.Shared/DependencyInjection/SToolkitServiceProvider.cs
@@ -1,10 +1,13 @@
-﻿namespace Community.VisualStudio.Toolkit.Shared.DependencyInjection
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace Community.VisualStudio.Toolkit.Shared.DependencyInjection
 {
     /// <summary>
     /// Placeholder interface for registering the <see cref="IToolkitServiceProvider{TPackage}"/> in the main Visual Studio service provider.
     /// </summary>
     /// <typeparam name="TPackage">Type of the implementing package.</typeparam>
     public interface SToolkitServiceProvider<TPackage>
+        where TPackage : AsyncPackage
     {
     }
 }
