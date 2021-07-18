0b2ef10e1a7238653b09ef49edca49b35dfcdb91
reduckted <reduckted@outlook.com>
2021-07-18 21:08:15 +1000

    Added the ability to change the theme from the Theme tool Window.

--- test/VSSDK.TestExtension/ToolWindows/ThemeWindow.cs
+++ test/VSSDK.TestExtension/ToolWindows/ThemeWindow.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using Community.VisualStudio.Toolkit;
 using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell;
-using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell.Settings;
 
 namespace TestExtension
 {
@@ -16,9 +18,41 @@ namespace TestExtension
 
         public override Type PaneType => typeof(Pane);
 
-        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        public async override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
         {
-            return Task.FromResult<FrameworkElement>(new ThemeWindowControl { DataContext = new ThemeWindowControlViewModel() });
+            Guid currentTheme = await GetCurrentThemeAsync(cancellationToken);
+
+            return new ThemeWindowControl { DataContext = new ThemeWindowControlViewModel(currentTheme) };
+        }
+
+        private async Task<Guid> GetCurrentThemeAsync(CancellationToken cancellationToken)
+        {
+            const string COLLECTION_NAME = @"ApplicationPrivateSettings\Microsoft\VisualStudio";
+            const string PROPERTY_NAME = "ColorTheme";
+
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var manager = (IVsSettingsManager)await VS.Services.GetSettingsManagerAsync();
+            SettingsStore store = new ShellSettingsManager(manager).GetReadOnlySettingsStore(SettingsScope.UserSettings);
+
+            if (store.CollectionExists(COLLECTION_NAME))
+            {
+                if (store.PropertyExists(COLLECTION_NAME, PROPERTY_NAME))
+                {
+                    // The value is made up of three parts, separated
+                    // by a star. The third part is the GUID of the theme.
+                    var parts = store.GetString(COLLECTION_NAME, PROPERTY_NAME).Split('*');
+                    if (parts.Length == 3)
+                    {
+                        if (Guid.TryParse(parts[2], out Guid value))
+                        {
+                            return value;
+                        }
+                    }
+                }
+            }
+
+            return Guid.Empty;
         }
 
         [Guid("e3be6dd3-f017-4d6e-ae88-2b29319a77a2")]
--- test/VSSDK.TestExtension/ToolWindows/ThemeWindowControlViewModel.cs
+++ test/VSSDK.TestExtension/ToolWindows/ThemeWindowControlViewModel.cs
@@ -1,11 +1,60 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Community.VisualStudio.Toolkit;
 using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
 
 namespace TestExtension
 {
     public class ThemeWindowControlViewModel : ObservableObject
     {
+        private Theme _selectedTheme;
         private bool _useVsTheme = true;
+
+        public ThemeWindowControlViewModel(Guid currentTheme)
+        {
+            Themes = new[] {
+                new Theme {
+                    Name = "Dark",
+                    Guid = new Guid("{1DED0138-47CE-435E-84EF-9EC1F439B749}"),
+                    ResourceName = "Theme.Dark.vssettings"
+                },
+                new Theme {
+                    Name = "Light",
+                    Guid = new Guid("{DE3DBBCD-F642-433C-8353-8F1DF4370ABA}"),
+                    ResourceName = "Theme.Light.vssettings"
+                },
+                new Theme {
+                    Name = "Blue",
+                    Guid = new Guid("{A4D6A176-B948-4B29-8C66-53C97A1ED7D0}"),
+                    ResourceName = "Theme.Blue.vssettings"
+                },
+                new Theme {
+                    Name = "Blue (Extra Contrast)",
+                    Guid = new Guid("{CE94D289-8481-498B-8CA9-9B6191A315B9}"),
+                    ResourceName = "Theme.BlueHighContrast.vssettings"
+                }
+            };
+
+            _selectedTheme = Themes.FirstOrDefault((x) => x.Guid.Equals(currentTheme)) ?? Themes[0];
+        }
+
+        public IReadOnlyList<Theme> Themes { get; }
+
+        public Theme SelectedTheme
+        {
+            get { return _selectedTheme; }
+            set
+            {
+                SetProperty(ref _selectedTheme, value);
+                ApplyThemeAsync(value).FireAndForget();
+            }
+        }
 
         public bool UseVsTheme
         {
@@ -22,5 +71,38 @@ namespace TestExtension
             ThemeWindowDialog dialog = new ThemeWindowDialog { DataContext = new ThemeWindowDialogViewModel { UseVsTheme = UseVsTheme } };
             dialog.ShowModal();
         }
+
+        private async Task ApplyThemeAsync(Theme theme)
+        {
+            // There doesn't appear to be a way to set the theme programatically, 
+            // but we can change the theme by using a pre-defined settings file and importing those settings.
+            // The settings files were created by exporting only the "Options/General/Font and Colors" setting.
+            var tempFileName = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("n") + ".vssettings");
+
+            using (Stream resource = GetType().Assembly.GetManifestResourceStream($"TestExtension.Resources.{theme.ResourceName}"))
+            {
+                using (FileStream file = System.IO.File.Open(tempFileName, FileMode.Create, FileAccess.Write))
+                {
+                    await resource.CopyToAsync(file);
+                }
+            }
+
+            try
+            {
+                await KnownCommands.Tools_ImportandExportSettings.ExecuteAsync($@"/import:""{tempFileName}""");
+            }
+            finally
+            {
+                System.IO.File.Delete(tempFileName);
+            }
+        }
+
+        public class Theme
+        {
+            public string Name { get; set; }
+            public Guid Guid { get; set; }
+            public string ResourceName { get; set; }
+        }
+
     }
 }
