688e6dbef7215a2f9116dfd5655c656cebe8c68e
reduckted <reduckted@outlook.com>
2021-07-19 21:02:21 +1000

    Added progress bar styles.

--- test/VSSDK.TestExtension/ToolWindows/ThemeWindow/ThemeWindowDemo.xaml.cs
+++ test/VSSDK.TestExtension/ToolWindows/ThemeWindow/ThemeWindowDemo.xaml.cs
@@ -1,13 +1,29 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Controls;
+using Microsoft.VisualStudio.Shell;
 
 namespace TestExtension
 {
-    public partial class ThemeWindowDemo : UserControl
+    public partial class ThemeWindowDemo : UserControl, INotifyPropertyChanged
     {
+        private double _progress;
+
         public ThemeWindowDemo()
         {
             InitializeComponent();
+            UpdateProgressAsync().FireAndForget();
+        }
+
+        private async Task UpdateProgressAsync()
+        {
+            while (true)
+            {
+                await Task.Delay(250).ConfigureAwait(true);
+                Progress = (Progress + 5) % 101;
+            }
         }
 
         public IEnumerable<string> ListItems { get; } = new string[] {
@@ -17,7 +33,27 @@ namespace TestExtension
                 "Fourth",
                 "Fifth",
                 "Sixth"
-       };
+        };
 
+        public double Progress
+        {
+            get
+            {
+                return _progress;
+            }
+            set
+            {
+                _progress = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
