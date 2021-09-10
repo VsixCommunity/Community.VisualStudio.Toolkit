6eec69076faeb4d6a4d801ad592438f9474cd8e3
reduckted <reduckted@outlook.com>
2021-09-10 19:29:55 +1000

    Created a class for accessing and manipulating Solution Explorer.

---
+++ test/VSSDK.TestExtension/Commands/CollapseSelectedItemsCommand.cs
@@ -0,0 +1,21 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace TestExtension
+{
+    [Command(PackageIds.CollapseSelectedItems)]
+    internal sealed class CollapseSelectedItemsCommand : BaseCommand<CollapseSelectedItemsCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            SolutionExplorerWindow solutionExplorer = await VS.Windows.GetSolutionExplorerWindowAsync();
+            if (solutionExplorer != null)
+            {
+                solutionExplorer.Collapse(await solutionExplorer.GetSelectionAsync());
+            }
+        }
+    }
+}
---
+++ test/VSSDK.TestExtension/Commands/ToggleVsixManifestFilterCommand.cs
@@ -0,0 +1,28 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace TestExtension
+{
+    [Command(PackageIds.ToggleVsixManifestFilter)]
+    internal sealed class ToggleVsixManifestFilterCommand : BaseCommand<ToggleVsixManifestFilterCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            SolutionExplorerWindow solutionExplorer = await VS.Windows.GetSolutionExplorerWindowAsync();
+            if (solutionExplorer != null)
+            {
+                if (solutionExplorer.IsFilterEnabled<VsixManifestFilterProvider>())
+                {
+                    solutionExplorer.DisableFilter();
+                }
+                else
+                {
+                    solutionExplorer.EnableFilter<VsixManifestFilterProvider>();
+                }
+            }
+        }
+    }
+}
