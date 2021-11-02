b2f55940371e33cd81e39b9f6cd613040386bf38
reduckted <reduckted@outlook.com>
2021-11-02 20:42:44 +1000

    Added an easier way to get loaded and/or unloaded project hierarchies.

---
+++ src/toolkit/Community.VisualStudio.Toolkit.Shared/Solution/ProjectStateFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Community.VisualStudio.Toolkit
+{
+    /// <summary>
+    /// Defines the different project states that can be used to filter projects.
+    /// </summary>
+    [Flags]
+    public enum ProjectStateFilter
+    {
+        /// <summary>
+        /// No projects.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Projects that are currently loaded.
+        /// </summary>
+        Loaded = 1,
+        /// <summary>
+        /// Projects that are currently unloaded.
+        /// </summary>
+        Unloaded = 2,
+        /// <summary>
+        /// Both loaded and unloaded projects.
+        /// </summary>
+        All = Loaded | Unloaded
+    }
+}
