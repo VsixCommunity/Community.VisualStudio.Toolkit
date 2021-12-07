ef4c2cb49f92714473254f76f1c20bc5dabd1298
reduckted <reduckted@outlook.com>
2021-12-07 20:35:50 +1000

    Added commands to the demo extension to load and unload projects.

---
+++ demo/VSSDK.TestExtension/Commands/UnloadSelectedProject.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+
+namespace TestExtension.Commands
+{
+    [Command(PackageIds.UnloadSelectedProject)]
+    internal sealed class UnloadSelectedProjectCommand : BaseCommand<UnloadSelectedProjectCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            Project project = await VS.Solutions.GetActiveProjectAsync();
+            if (project != null)
+            {
+                await project.UnloadAsync();
+
+                // Show a message to demonstrate that the Project object can still be used.
+                await VS.MessageBox.ShowAsync($"Project '{project.Name}' has been unloaded.");
+            }
+        }
+    }
+}
