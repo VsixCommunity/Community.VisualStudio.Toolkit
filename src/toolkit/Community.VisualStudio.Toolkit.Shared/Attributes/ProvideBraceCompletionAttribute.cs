430ce5ee98e38cec86607f204b07703613abd6ff
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-12-21 10:57:31 -0800

    Fixed the shared project items [release]

--- src/toolkit/Community.VisualStudio.Toolkit.Shared/Attributes/ProvideBraceCompletionAttribute.cs
+++ src/toolkit/Community.VisualStudio.Toolkit.Shared/Attributes/ProvideBraceCompletionAttribute.cs
@@ -1,28 +1,40 @@
 ﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
 
 namespace Community.VisualStudio.Toolkit
 {
+    /// <summary>
+    /// Adds support for brace completion to the specified language.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
     public class ProvideBraceCompletionAttribute : RegistrationAttribute
     {
         private readonly string _languageName;
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="languageName">The language name to add brace completion to.</param>
         public ProvideBraceCompletionAttribute(string languageName)
         {
             _languageName = languageName;
         }
 
+        /// <inheritdoc/>
         public override void Register(RegistrationContext context)
         {
-            var keyName = string.Format(CultureInfo.InvariantCulture, "{0}\\{1}\\{2}", "Languages", "Language Services", _languageName);
+            string keyName = string.Format(CultureInfo.InvariantCulture, "{0}\\{1}\\{2}", "Languages", "Language Services", _languageName);
             using (Key langKey = context.CreateKey(keyName))
             {
                 langKey.SetValue("ShowBraceCompletion", 1);
             }
         }
 
+        /// <inheritdoc/>
         public override void Unregister(RegistrationContext context)
         {
-            var keyName = string.Format(CultureInfo.InvariantCulture, "{0}\\{1}\\{2}", "Languages", "Language Services", _languageName);
+            string keyName = string.Format(CultureInfo.InvariantCulture, "{0}\\{1}\\{2}", "Languages", "Language Services", _languageName);
             context.RemoveKey(keyName);
         }
     }
