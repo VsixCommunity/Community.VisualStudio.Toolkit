5c4947a1dbd16a6df5797eefb0c91e3ad766bd79
reduckted <reduckted@outlook.com>
2023-02-09 21:43:14 +1000

    Created a tool window to demonstrate the font and color services.

---
+++ demo/VSSDK.TestExtension/ToolWindows/FontsAndColors/DemoFontAndColorProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Community.VisualStudio.Toolkit;
+
+namespace TestExtension
+{
+    [Guid("d2bc5f5f-6c24-4f6c-b6ef-aea775be5fa4")]
+    public class DemoFontAndColorProvider : BaseFontAndColorProvider { }
+}
---
+++ demo/VSSDK.TestExtension/ToolWindows/FontsAndColors/FontsAndColorsWindowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+using Community.VisualStudio.Toolkit;
+
+namespace TestExtension
+{
+    public partial class FontsAndColorsWindowControl : UserControl
+    {
+        public FontsAndColorsWindowControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyColor(Border border, ConfiguredColor color)
+        {
+            // Bind the border's properties to the configured
+            // color properties. This could also be done in XAML.
+            border.DataContext = color;
+            border.SetBinding(BackgroundProperty, nameof(ConfiguredColor.BackgroundBrush));
+            border.SetBinding(TextElement.ForegroundProperty, nameof(ConfiguredColor.ForegroundBrush));
+            border.SetBinding(TextElement.FontWeightProperty, nameof(ConfiguredColor.FontWeight));
+        }
+    }
+}
